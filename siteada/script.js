f3ab4f1c0ca407445c3214af3795f1861a23aab3
Daniele Eloi <danieleeloi@gmail.com>
2021-03-08 18:43:22 -0300

    Alteração links divas
    Acrescentado hover

--- siteada/script.js
+++ siteada/script.js
@@ -1,6 +1,17 @@
 
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
+/*var nome = document.getElementById("nome").value;
+var email = document.getElementById("email").value;
+var tel = document.getElementById("tel").value;
+
+function validaFormulario(){
+    if(nome != "" && email != "" && tel != ""){
+        alert("Prontinho! Você receberá as novidades por email")
+    }else{
+        alert("Por favor, preencha os campos nome, email e telefone!")
+    }
+}*/
 function validaFormulario(){
     if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("tel").value != "") {
         alert("Prontinho! Você receberá as novidades por email")
